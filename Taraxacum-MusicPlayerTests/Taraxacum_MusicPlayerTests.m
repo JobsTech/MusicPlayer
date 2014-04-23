cf25319e2b9915c566e915f5598405c4dd436894
azxfire <azxfire@gmail.com>
2014-04-22 22:07:19 -0700

    zsczcsz
    zsczscz

---
+++ Taraxacum-MusicPlayerTests/Taraxacum_MusicPlayerTests.m
@@ -0,0 +1,34 @@
+//
+//  Taraxacum_MusicPlayerTests.m
+//  Taraxacum-MusicPlayerTests
+//
+//  Created by wangtao on 14-4-22.
+//  Copyright (c) 2014年 CCAT.COM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Taraxacum_MusicPlayerTests : XCTestCase
+
+@end
+
+@implementation Taraxacum_MusicPlayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
