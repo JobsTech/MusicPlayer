1673016fbf2bdeb43825821352158dc252f8ffbd
azxfire <azxfire@gmail.com>
2014-04-22 22:49:33 -0700

    dog

---
+++ MusicPlayer/MusicPlayer/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  MusicPlayer
+//
+//  Created by wangtao on 14-4-22.
+//  Copyright (c) 2014年 CCAT.COM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
