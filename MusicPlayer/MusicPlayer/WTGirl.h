ff62ebb5b442ea5ba677ad6b38ceefcb43e6dfe5
azxfire <azxfire@gmail.com>
2014-04-23 22:27:32 -0700

    产生为那个了gril类

---
+++ MusicPlayer/MusicPlayer/WTGirl.h
@@ -0,0 +1,13 @@
+//
+//  WTGirl.h
+//  MusicPlayer
+//
+//  Created by wangtao on 14-4-23.
+//  Copyright (c) 2014年 CCAT.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTGirl : UIView
+
+@end
---
+++ MusicPlayer/MusicPlayer/WTGirl.m
@@ -0,0 +1,31 @@
+//
+//  WTGirl.m
+//  MusicPlayer
+//
+//  Created by wangtao on 14-4-23.
+//  Copyright (c) 2014年 CCAT.COM. All rights reserved.
+//
+
+#import "WTGirl.h"
+
+@implementation WTGirl
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
