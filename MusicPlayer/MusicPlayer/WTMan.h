968f76cbfedaac2f093722aa1417f2ce26e49bb4
azxfire <azxfire@gmail.com>
2014-04-23 21:27:01 -0700

    test-x

---
+++ MusicPlayer/MusicPlayer/WTMan.h
@@ -0,0 +1,13 @@
+//
+//  WTMan.h
+//  MusicPlayer
+//
+//  Created by wangtao on 14-4-23.
+//  Copyright (c) 2014年 CCAT.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTMan : UIView
+
+@end
