3674ae9036a6a4e48ac15045f7a7ee2f6f572916
azxfire <azxfire@gmail.com>
2014-04-23 22:31:32 -0700

    Testlei

---
+++ MusicPlayer/MusicPlayer/WTTest.h
@@ -0,0 +1,13 @@
+//
+//  WTTest.h
+//  MusicPlayer
+//
+//  Created by wangtao on 14-4-23.
+//  Copyright (c) 2014年 CCAT.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTTest : UIView
+
+@end
---
+++ MusicPlayer/MusicPlayer/WTTest.m
@@ -0,0 +1,31 @@
+//
+//  WTTest.m
+//  MusicPlayer
+//
+//  Created by wangtao on 14-4-23.
+//  Copyright (c) 2014年 CCAT.COM. All rights reserved.
+//
+
+#import "WTTest.h"
+
+@implementation WTTest
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
